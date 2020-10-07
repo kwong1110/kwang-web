e4e1f8044cea5cb3017505b5e2867c193678fe2d
kwong1110 <54402625+kwong1110@users.noreply.github.com>
2020-10-07 17:56:47 +0900

    heroku 배포 진행중 ..

--- server/index.js
+++ server/index.js
@@ -33,14 +33,6 @@ app.use(cookieParser());
 app.use("/api/users", require("./routes/users"));
 app.use("/api/posts", require("./routes/posts"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
-
-app.listen(port, () => {
+app.listen(app.get("port"), () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
