5fdb56a613292ac22e331753f7697fe1b1c619a1
kwong1110 <54402625+kwong1110@users.noreply.github.com>
2020-10-08 22:01:20 +0900

    heroku test

--- server/index.js
+++ server/index.js
@@ -36,13 +36,15 @@ app.use("/api/posts", require("./routes/posts"));
 app.get("/", (req, res) => {
   res.send(`Heroku deploy !! ${port}`);
 });
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build/"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "public", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build/"));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
+
 app.listen(app.get("port"), () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
