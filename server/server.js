38c9c9e84f1841eaa083c68c1f90510c925e4b26
kwong1110 <54402625+kwong1110@users.noreply.github.com>
2020-10-08 22:14:37 +0900

    server/index.js -> server.js 파일명 변경

--- server/server.js
+++ server/server.js
@@ -37,13 +37,13 @@ app.get("/", (req, res) => {
   res.send(`Heroku deploy !! ${port}`);
 });
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build/"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build/"));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(app.get("port"), () => {
   console.log(`Example app listening at http://localhost:${port}`);
