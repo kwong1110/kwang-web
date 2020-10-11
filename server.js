01451603fdb5175b748b232566c7ba027fbfbfc5
kwong1110 <54402625+kwong1110@users.noreply.github.com>
2020-10-11 19:11:37 +0900

    router err 해결중 ..

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.use("/api/users", require("./server/routes/users"));
 app.use("/api/posts", require("./server/routes/posts"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "build")));
 
-  app.get("/ *", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
   });
 }
